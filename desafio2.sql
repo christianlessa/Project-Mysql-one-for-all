e47a029e4dc770fed0c41b0bf22000f29c9e3747
Christian Lessa <christianclessa@gmail.com>
2022-03-09 18:12:33 -0300

    Requisito 2 concluido

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+SELECT 
+(SELECT COUNT(song_id) FROM SpotifyClone.songs) AS 'cancoes',
+(SELECT COUNT(artist_id) FROM SpotifyClone.artist) AS 'artistas',
+(SELECT COUNT(album_id) FROM SpotifyClone.albums) AS 'albuns';
