a33da1003804ffd46bddcd296fc2f18b426d518a
Christian Lessa <christianclessa@gmail.com>
2022-03-15 00:43:50 -0300

    Requisito 8 concluido

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT ar.artist_name AS artista,
+al.album_name AS album
+FROM SpotifyClone.artist AS ar
+INNER JOIN SpotifyClone.albums AS al
+ON ar.artist_id = al.artist_id
+WHERE ar.artist_name = 'Walter Phoenix'
+ORDER BY album;
